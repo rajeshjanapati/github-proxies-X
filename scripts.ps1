7919e6ede31eb67328d8d30c3368320cb8b61ba6
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-04 16:24:18 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -15,28 +15,8 @@ $orgName = "esi-apigee-x-394004"
 $envName = "eval"
 $apiKey = "Bearer $token"
 
-# # Clone the repository
-# git clone https://github.com/rajeshjanapati/apigee-artifacts.git
-# # cd $repositoryName
-# cd apigee/artifacts-nonprod/proxies
-
-# # Read JSON files
-# $jsonFiles = Get-ChildItem -Filter *.json -Recurse
-
-# $jsonContent = Get-Content -Path $jsonFile -Raw
-# # Parse the JSON content
-# $jsonData = ConvertFrom-Json $jsonContent
-
-# # Extract the value of the "name" key from the JSON data
-# $proxyName = $jsonData.name
-
-# Write-Host "Proxy-Name: $proxyName"
-
-# Define the list of GitHub file URLs to download
-$githubFileUrls = @(
-    "https://github.com/$repositoryOwner/$repositoryName/zipball/release/org"
-    # Add more file URLs as needed
-)
+# Define the GitHub API endpoint to get the ZIP file
+$githubApiUrl = "https://api.github.com/repos/$repositoryOwner/$repositoryName/zipball/$branchName"
 
 # Create headers with the GitHub authentication token and User-Agent
 $githubHeaders = @{
@@ -44,44 +24,37 @@ $githubHeaders = @{
     "User-Agent" = "PowerShell-GitHub-Downloader"
 }
 
-# Loop through each GitHub file URL
-foreach ($githubFileUrl in $githubFileUrls) {
-    # Generate a unique name for the downloaded ZIP file
-    $zipFileName = "downloaded_$(Get-Date -Format 'yyyyMMddHHmmss').zip"
-    $zipFilePath = Join-Path -Path $env:TEMP -ChildPath $zipFileName
+# Define the path to save the downloaded ZIP file
+$zipFilePath = "downloaded.zip"
 
-    # Download the ZIP file from GitHub
-    Invoke-WebRequest -Uri $githubFileUrl -Headers $githubHeaders -OutFile $zipFilePath
+# Download the ZIP file from GitHub
+Invoke-WebRequest -Uri $githubApiUrl -Headers $githubHeaders -OutFile $zipFilePath
 
-    # Check if the ZIP file download was successful
-    if (Test-Path -Path $zipFilePath) {
-        Write-Host "ZIP file downloaded successfully from GitHub: $zipFileName"
+# Check if the ZIP file download was successful
+if (Test-Path -Path $zipFilePath) {
+    Write-Host "ZIP file downloaded successfully from GitHub."
 
-        # Define the Apigee X API endpoint to upload the ZIP file
-        $apigeeApiUrl = "https://apigee.googleapis.com/v1/organizations/$orgName/environments/$envName/deployments"
+    # Define the Apigee X API endpoint to upload the ZIP file
+    $apigeeApiUrl = "https://apigee.googleapis.com/v1/organizations/$orgName/environments/$envName/deployments"
 
-        # Create headers with the Apigee X API key and Content-Type
-        $apigeeHeaders = @{
-            "x-api-key" = $apiKey
-            "Content-Type" = "application/octet-stream"
-        }
-
-        # Read the contents of the ZIP file
-        $zipFileContent = [System.IO.File]::ReadAllBytes($zipFilePath)
-
-        # Send a POST request to upload the ZIP file to Apigee X
-        $apigeeResponse = Invoke-RestMethod -Uri $apigeeApiUrl -Method 'POST' -Headers $apigeeHeaders -Body $zipFileContent
-
-        # Check if the ZIP file upload was successful
-        if ($apigeeResponse -ne $null) {
-            Write-Host "ZIP file uploaded successfully to Apigee X."
-        } else {
-            Write-Host "Failed to upload the ZIP file to Apigee X."
-        }
-
-        # Remove the downloaded ZIP file to avoid cluttering the system
-        # Remove-Item -Path $zipFilePath -Force
-    } else {
-        Write-Host "Failed to download the ZIP file from GitHub."
+    # Create headers with the Apigee X API key and Content-Type
+    $apigeeHeaders = @{
+        "x-api-key" = $apiKey
+        "Content-Type" = "application/octet-stream"
     }
+
+    # Read the contents of the ZIP file
+    $zipFileContent = [System.IO.File]::ReadAllBytes($zipFilePath)
+
+    # Send a POST request to upload the ZIP file to Apigee X
+    $apigeeResponse = Invoke-RestMethod -Uri $apigeeApiUrl -Method 'POST' -Headers $apigeeHeaders -Body $zipFileContent
+
+    # Check if the ZIP file upload was successful
+    if ($apigeeResponse -ne $null) {
+        Write-Host "ZIP file uploaded successfully to Apigee X."
+    } else {
+        Write-Host "Failed to upload the ZIP file to Apigee X."
+    }
+} else {
+    Write-Host "Failed to download the ZIP file from GitHub."
 }
