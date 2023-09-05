7d960187a8cac32a54d4df40f580177ebf59a0c6
rajeshjanapati <rajeshjanapati@gmail.com>
2023-09-05 09:51:00 +0530

    33

--- scripts.ps1
+++ scripts.ps1
@@ -10,12 +10,10 @@ $repositoryName = "github-proxies-X"
 $branchName = "main"  # Change this to the branch you want to access
 $githubToken = "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"
 
+# ------------------------------------Proxies------------------------------------------
 # Clone the repository
 git clone https://github.com/rajeshjanapati/github-proxies-X.git
 # cd $repositoryName
-
-# Set the directory path where you want to search for .zip files
-# $directoryPath = "$repositoryName"
 
 # Use Get-ChildItem to list all files in the directory with a .zip extension
 $zipFiles = Get-ChildItem -Path $directoryPath -Filter *.zip
@@ -55,7 +53,7 @@ foreach ($zipFile in $zipFiles) {
     # Print the filename without the extension
     Write-Host "Filename without extension: $filenameWithoutExtension"
 
-    $uploadurl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/apis?name="+$filenameWithoutExtension+"&action=import"
+    $uploadurl = "https://apigee.googleapis.com/v1/organizations/"+$org+"/apis?name="+$filenameWithoutExtension+"&action=import"
 
     Write-Host $uploadurl
 
@@ -64,115 +62,94 @@ foreach ($zipFile in $zipFiles) {
 
 }
 
-# Set your Apigee X organization and environment
-$orgName = "esi-apigee-x-394004"
-$envName = "eval"
-$apiKey = "Bearer $token"
+# --------------------------------------KVMs-------------------------------------------
 
-# # Define the GitHub API endpoint to get the ZIP file
-# $githubApiUrl = "https://api.github.com/repos/$repositoryOwner/$repositoryName/zipball/$branchName"
+cd kvms
 
-# # Create headers with the GitHub authentication token and User-Agent
-# $githubHeaders = @{
-#     Authorization = "Bearer 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"
-#     # "User-Agent" = "PowerShell-GitHub-Downloader"
-#     "User-Agent" = ".github/workflows"
-#     # "USER_AGENT" = ".github/workflows"
-#     # "Accept"="application/vnd.github+json"
-# }
+# Read JSON files
+$jsonFiles = Get-ChildItem -Filter *.json -Recurse
 
+# Loop through each JSON file and make POST requests
+foreach ($jsonFile in $jsonFiles) {
+    $jsonContent = Get-Content -Path $jsonFile -Raw
+    # Parse the JSON content
+    $jsonData = ConvertFrom-Json $jsonContent
 
-# Define the path to save the downloaded ZIP file
-$zipFilePath = "esi-apigee-x-394004-proxy-FLSessionPreFilter-rev5.zip"
+    # Extract the value of the "name" key from the JSON data
+    $kvmName = $jsonData.name
 
-# # Download the ZIP file from GitHub
-# Invoke-WebRequest -Uri $githubApiUrl -Headers $githubHeaders -OutFile $zipFilePath
+    # Print the extracted value
+    # Write-Host "KVM Name: $kvmName"
 
-# # Check if the ZIP file download was successful
-# if (Test-Path -Path $zipFilePath) {
-#     Write-Host "ZIP file downloaded successfully from GitHub."
-
-#     # Define the Apigee X API endpoint to upload the ZIP file
-#     $apigeeApiUrl = "https://apigee.googleapis.com/v1/organizations/$orgName/"
-
-#     # Create headers with the Apigee X API key and Content-Type
-#     $apigeeHeaders = @{
-#         "x-api-key" = "Bearer $token"
-#         "Content-Type" = "application/octet-stream"
-#     }
-
-#     # Read the contents of the ZIP file
-#     $zipFileContent = [System.IO.File]::ReadAllBytes($zipFilePath)
-
-#     # Send a POST request to upload the ZIP file to Apigee X
-#     $apigeeResponse = Invoke-RestMethod -Uri "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/apis?name=$zipFilePath&action=import" -Method 'POST' -Headers $apigeeHeaders -Body $zipFileContent
-
-#     # Check if the ZIP file upload was successful
-#     if ($apigeeResponse -ne $null) {
-#         Write-Host "ZIP file uploaded successfully to Apigee X."
-#     } else {
-#         Write-Host "Failed to upload the ZIP file to Apigee X."
-#     }
-# } else {
-#     Write-Host "Failed to download the ZIP file from GitHub."
-# }
-
-# ------------------POSTMAN------------------------------------
-
-# $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-# $headers.Add("Authorization", "Bearer $token")
-
-# $multipartContent = [System.Net.Http.MultipartFormDataContent]::new()
-# $multipartFile = $zipFilePath
-# $FileStream = [System.IO.FileStream]::new($multipartFile, [System.IO.FileMode]::Open)
-# $fileHeader = [System.Net.Http.Headers.ContentDispositionHeaderValue]::new("form-data")
-# $fileHeader.Name = "file"
-# $fileHeader.FileName = $zipFilePath
-# $fileContent = [System.Net.Http.StreamContent]::new($FileStream)
-# $fileContent.Headers.ContentDisposition = $fileHeader
-# $multipartContent.Add($fileContent)
-
-# $body = $multipartContent
-
-# $response = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/apis?name=esi-apigee-x-394004-proxy-FLSessionPreFilter-rev5&action=import' -Method 'POST' -Headers $headers -Body $body
-# $response | ConvertTo-Json
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $token")
+    $headers.Add("Content-Type", "application/json")
 
 
+    $kvmget = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'GET' -Headers $headers
+    $kvmget | ConvertTo-Json
+    # Write-Host $kvmget
 
-# --------------------------------------------------------------
+    $url = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/$kvmName/entries"
+
+    $kvmgetentries = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $headers
+    $kvmgetentriesvalues = $kvmgetentries | ConvertTo-Json
+    # Write-Host $kvmgetentriesvalues
+    
+    
+    # # Output the KVM entries for debugging
+    $kvmgetentriesvalues | Format-Table
+
+    # Your array
+    $array = $kvmget
+    
+    foreach ($valueToCheck in $array) {
+        if ($array -contains $valueToCheck) {
+            Write-Host "$valueToCheck is present in the array."
+            $entries = $jsonData.entry
+            Write-Host "Values: $vlaues"
+        
+            foreach ($entry in $entries) {
+                Write-Host "step-2"
+                $name = $entry.key
+                $value = $entry.value
+                Write-Host "Key: $name, Value: $value"
+                $body2 = @{
+                    "name" = $name;
+                    "value" = $value;
+                }
+                Write-Host "body2: $body2"
+                
+                try {
+                    
+                    # Make the API request
+                    $response = Invoke-RestMethod -Uri "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/$kvmName/entries" -Method 'POST' -Headers $headers -Body ($body2 | ConvertTo-Json)
+                
+                    # Get and print the status code
+                    $statuscode = $response.StatusCode
+                    Write-Host "Status Code: $statuscode"
+                } catch [System.Net.HttpStatusCode] {
+                    # Handle the specific error (HTTP status code 409) gracefully
+                    Write-Host "Conflict (409) error occurred, but the script will continue."
+                } catch {
+                    # Handle any other exceptions that may occur
+                    Write-Host "An error occurred: $_"
+                }
+
+            }
+        } else {
+            $body1 =@{
+                "name"=$kvmName;
+                "encrypted"=true;
+                }
+            Write-Host $body1
+            Write-Host "$valueToCheck is not present in the array."
+            $kvmcreate = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'POST' -Headers $headers -Body ($body1|ConvertTo-Json)
+            $kvmcreate | ConvertTo-Json
+        }
+    }
+}
+cd ..
 
 
-
-# # Set your GitHub repository information
-# $owner = "rajeshjanapati@gmail.com"
-# $repo = "github-proxies-X"
-# $filePath = "scripts.ps1" # Path to the file you want to download
-
-# # Set your GitHub token (PAT or GitHub Actions token)
-# $token = "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"
-
-# # Create headers with the GitHub authentication token and User-Agent
-# $headers = @{
-#     Authorization = "Bearer $token"
-#     "User-Agent" = ".github/workflows/github-proxies-X"  # Replace with your own User-Agent
-# }
-
-# # Define the GitHub API URL to get the contents of the file
-# $githubApiUrl = "https://api.github.com/repos/$owner/$repo/contents/$filePath"
-
-# # Make an HTTP GET request to the GitHub API
-# $fileContent = Invoke-RestMethod -Uri $githubApiUrl -Method 'GET' -Headers $headers
-
-# # Check if the request was successful
-# if ($fileContent -ne $null) {
-#     # The file content is base64-encoded, so decode it
-#     $decodedContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($fileContent.content))
-
-#     # Save the file content to a local file
-#     $localFilePath = "downloaded_file.ext"  # Choose a local file path and name
-#     Set-Content -Path $localFilePath -Value $decodedContent -Encoding UTF8
-
-#     Write-Host "File downloaded successfully to $localFilePath"
-# } else {
-#     Write-Host "Failed to download the file from GitHub."
-# }
+# -------------------------------------------------------------------------------------
