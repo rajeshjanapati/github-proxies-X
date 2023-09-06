4405e9a9623991bec05e40f2a344b0773fbd2e1d
rajeshjanapati <rajeshjanapati@gmail.com>
2023-09-06 15:03:53 +0530

    22

--- scripts.ps1
+++ scripts.ps1
@@ -89,17 +89,7 @@ foreach ($jsonFile in $jsonFiles) {
     $kvmget = Invoke-RestMethod -Uri "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps" -Method 'GET' -Headers $headers
     $kvmget | ConvertTo-Json
     Write-Host $kvmget
-
-    $url = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/"+$kvmName+"/entries"
-    Write-Host $url
-
-    $kvmgetentries = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $headers
-    $kvmgetentriesvalues = $kvmgetentries | ConvertTo-Json
-    # Write-Host $kvmgetentriesvalues
     
-    
-    # # Output the KVM entries for debugging
-    $kvmgetentriesvalues | Format-Table
 
     # Your array
     $array = $kvmget
@@ -109,6 +99,17 @@ foreach ($jsonFile in $jsonFiles) {
             Write-Host "$valueToCheck is present in the array."
             $entries = $jsonData.entry
             Write-Host "Values: $vlaues"
+
+            # $url = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/"+$kvmName+"/entries"
+            # Write-Host $url
+
+            # $kvmgetentries = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $headers
+            # $kvmgetentriesvalues = $kvmgetentries | ConvertTo-Json
+            # # Write-Host $kvmgetentriesvalues
+            
+            
+            # # # Output the KVM entries for debugging
+            # $kvmgetentriesvalues | Format-Table
         
             foreach ($entry in $entries) {
                 Write-Host "step-2"
@@ -147,6 +148,49 @@ foreach ($jsonFile in $jsonFiles) {
             Write-Host "$valueToCheck is not present in the array."
             $kvmcreate = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'POST' -Headers $headers -Body ($body1|ConvertTo-Json)
             $kvmcreate | ConvertTo-Json
+            
+            $entries = $jsonData.entry
+            Write-Host "Values: $vlaues"
+
+            # $url = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/"+$kvmName+"/entries"
+            # Write-Host $url
+
+            # $kvmgetentries = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $headers
+            # $kvmgetentriesvalues = $kvmgetentries | ConvertTo-Json
+            # # Write-Host $kvmgetentriesvalues
+            
+            
+            # # # Output the KVM entries for debugging
+            # $kvmgetentriesvalues | Format-Table
+        
+            foreach ($entry in $entries) {
+                Write-Host "step-2"
+                $name = $entry.key
+                $value = $entry.value
+                Write-Host "Key: $name, Value: $value"
+                $body2 = @{
+                    "name" = $name;
+                    "value" = $value;
+                }
+                Write-Host "body2: $body2"
+                
+                try {
+                    
+                    # Make the API request
+                    $response = Invoke-RestMethod -Uri "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/$kvmName/entries" -Method 'POST' -Headers $headers -Body ($body2 | ConvertTo-Json)
+                
+                    # Get and print the status code
+                    $statuscode = $response.StatusCode
+                    Write-Host "Status Code: $statuscode"
+                } catch [System.Net.HttpStatusCode] {
+                    # Handle the specific error (HTTP status code 409) gracefully
+                    Write-Host "Conflict (409) error occurred, but the script will continue."
+                } catch {
+                    # Handle any other exceptions that may occur
+                    Write-Host "An error occurred: $_"
+                }
+
+            }
         }
     }
 }
